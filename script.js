a0148376ff0c01dc81c5cc83874f4fe6a44b9376
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-06-17 09:12:52 -0300

    Projeto clone instagram

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var foto1 = document.getElementById("tela1")
+foto1.style.display="relative"
+var foto2 = document.getElementById("tela2")
+foto2.style.display="none"
+var foto3 = document.getElementById("tela3")
+foto3.style.display="none"
+var foto4 = document.getElementById("tela4")
+foto4.style.display="none"
+
+// let time = 2000,
+// currentImageIndex = 0,
+// images = document.querySelectorAll(".imgtela")
+// max = images.length;
+
+// function nextImage() {
+//  console.log(images)
+// }
+
+
+// function start () {
+//     setInterval(() =>{
+
+// nextImage()        
+//     }, time)
+// }
+
+
+window.onload = function () {
+     alert("Está carregado!") 
+
+
+
+
+
+
+     
+
+} 
+
